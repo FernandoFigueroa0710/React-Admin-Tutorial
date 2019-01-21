e5550ea0400c657a58eb769ad9cfb25c2b14a005
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-21 16:21:52 -0600

    update: passed props to component

--- src/users.js
+++ src/users.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { List, Datagrid, TextField, EmailField } from "react-admin";
 
-export const UserList = () => {
+export const UserList = props => {
   return (
-    <List>
+    <List {...props}>
       <Datagrid>
         <TextField source="id" />
         <TextField source="name" />

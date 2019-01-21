6debe375e7fc282293e7481a0096ecfd7ac82ef1
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-21 17:53:11 -0600

    update: imported post list component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./users";
+import { PostList } from "./posts";
 
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
 
 const App = () => {
   return (
     <Admin dataProvider={dataProvider}>
+      <Resource name="posts" list={PostList} />
       <Resource name="users" list={UserList} />
     </Admin>
   );

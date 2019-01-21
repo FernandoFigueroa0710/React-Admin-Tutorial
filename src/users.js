c8ce40d8f29e168239e53fdbb4f69dfd52f2ec06
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-21 16:13:58 -0600

    update: create users component

---
+++ src/users.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { List, Datagrid, TextField, EmailField } from "react-admin";
+
+export const UserList = () => {
+  return (
+    <List>
+      <Datagrid>
+        <TextField source="id" />
+        <TextField source="name" />
+        <TextField source="username" />
+        <EmailField source="email" />
+        <TextField source="address.street" />
+        <TextField source="phone" />
+        <TextField source="website" />
+        <TextField source="company.name" />
+      </Datagrid>
+    </List>
+  );
+};

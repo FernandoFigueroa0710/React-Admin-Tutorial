b0cd526e1f124a2185f741d9fbcd211b590c674d
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-21 22:15:08 -0600

    update: icons added to sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
 
@@ -14,8 +16,9 @@ const App = () => {
         list={PostList}
         edit={PostEdit}
         create={PostCreate}
+        icon={PostIcon}
       />
-      <Resource name="users" list={UserList} />
+      <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
   );
 };

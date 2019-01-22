ecf97d8d1868d75ff7ee772b2ebbee36494c24ee
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-21 21:57:31 -0600

    update: updated the resources post component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./users";
-import { PostList } from "./posts";
+import { PostList, PostEdit, PostCreate } from "./posts";
 
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
 
 const App = () => {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="posts" list={PostList} />
+      <Resource
+        name="posts"
+        list={PostList}
+        edit={PostEdit}
+        create={PostCreate}
+      />
       <Resource name="users" list={UserList} />
     </Admin>
   );
